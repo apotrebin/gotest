2ef9148de062b39feddd975b585365521f551db0
Akram Potrebin <akram.potrebin@zensoft.io>
2019-05-29 15:29:07 +0600

    structs

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName string
+	// contact contactInfo
+	contactInfo
+}
+
+type contactInfo struct {
+	email string
+	zipCode int 
+}
+
+func main(){
+
+	// var akram person
+	// akram.firstName = "Akram"
+	// akram.lastName = "Potrebin"
+
+	// akram := person{firstName: "Akram", lastName: "potrebin"}
+	// fmt.Println(akram)
+	// fmt.Printf("%+v", akram)
+
+
+	jim := person{
+		firstName: "Jim",
+		lastName: "Doe",
+		contactInfo: contactInfo{
+			email: "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	jim.updateName("Jimmy")
+	jim.print()
+}
+
+
+
+func (p person) print(){
+	fmt.Printf("%+v",p)
+}
+
+func (p person) updateName(newFirstName string){
+	p.firstName = newFirstName
+}
