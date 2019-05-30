4ea5a03679552fd8f132feaf0c996ebb803fd1d8
Akram <apotrebin@gmail.com>
2019-05-31 00:50:40 +0600

    interfaces

--- map/main.go
+++ map/main.go
@@ -9,18 +9,27 @@ func main() {
 	//var colors map[string]string
 
 	// ---------------  Case # 2
-	//colors := map[string]string{
-	//	"red":"#ff00000",
-	//	"green": "#ff333333",
-	//}
+	colors := map[string]string{
+		"red":   "#ff00000",
+		"green": "#ff333333",
+		"white": "dsasgdfgdfg",
+	}
 
 	// ---------------  Case # 3
-	colors := make(map[int]string)
+	//colors := make(map[int]string)
 
-	colors[111] = "#gg545454"
-	colors[222] = "#gg545454"
+	//colors[111] = "#gg545454"
+	//colors[222] = "#gg545454"
+	//
+	delete(colors, "sdfsdf")
 
-	delete(colors, 222)
+	//fmt.Println(colors)
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(m map[string]string) {
+	for k, v := range m {
+		fmt.Println("Hex is:", v, "and color is", k)
+	}
 }
