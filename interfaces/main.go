4e08c01714e0a30168320339522657ec71894be2
Akram Potrebin <akram.potrebin@zensoft.io>
2019-05-31 17:05:05 +0600

    work with interface

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,12 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"io"
+	"net/http" 
+)
 
 type bot interface {
 	getGreeting() string
 }
 type englishBot struct{}
 type spanishBot struct{}
+type logWriter struct{}
 
 func main() {
 	eb := englishBot{}
@@ -14,6 +20,26 @@ func main() {
 
 	printGreeting(eb)
 	printGreeting(sb)
+
+resp, err := http.Get("http://google.com")
+if err != nil{
+	fmt.Println("Error:", err)
+	os.Exit(1)
+}
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+	os.Exit(0)
+
+
+
+
+
+} // main func END -----------
+
+func (logWriter) Write(bs []byte) (int, error){
+	fmt.Println(string(bs))
+	fmt.Println("Just show length of the bytes", len(bs))
+	return len(bs), nil
 }
 
 func printGreeting(b bot) {
